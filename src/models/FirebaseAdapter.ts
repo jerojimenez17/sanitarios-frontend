3e81b1b2435d08a2b583f74f37f43ad738f370c3
jerojimenez17 <jerodepiran@gmail.com>
2023-02-06 03:28:43 -0300

    fix  id issue with converter

--- src/models/FirebaseAdapter.ts
+++ src/models/FirebaseAdapter.ts
@@ -3,12 +3,16 @@ import CartState from "./CartState";
 
 export class FirebaseAdapter {
   public static fromDocumentDataArray(data: DocumentData[]): CartState[] {
-    return data.map((d) => FirebaseAdapter.fromDocumentData(d.data()));
+    return data.map((d) => FirebaseAdapter.fromDocumentData(d.data(), d.id));
   }
 
-  public static fromDocumentData(data: DocumentData): CartState {
-    console.log(data.date);
+  public static fromDocumentData(
+    data: DocumentData,
+    dataId: string
+  ): CartState {
+    console.log(dataId);
     return {
+      id: dataId,
       products: data.products,
       total: data.total,
       totalWithDiscount: data.totalWithDiscount,
