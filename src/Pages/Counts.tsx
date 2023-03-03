ec660e736b468a3d909864d41eccd75fbc3597be
jerojimenez17 <jerodepiran@gmail.com>
2023-03-03 00:50:12 -0300

    change the view

--- src/Pages/Counts.tsx
+++ src/Pages/Counts.tsx
@@ -9,45 +9,34 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import { deleteDoc, doc, DocumentData } from "firebase/firestore";
+import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import React, { useCallback, useEffect, useState } from "react";
 import { db, fetchSales, updateProduct } from "../services/FireBase";
 import Row from "../components/Row";
 import { fetchProductById } from "../services/ProductService";
 import Product from "../models/Product";
 import CartState from "../models/CartState";
+import { FirebaseAdapter } from "../models/FirebaseAdapter";
 
 const Counts = () => {
   const [sales, setsales] = useState<CartState[] | null>([]);
-  const [newProductsState, setNewProductsState] = useState<any[]>([]);
-  const [newSalesState, setNewSalesState] = useState<CartState[]>([]);
   const [openDeleteModal, setOpenDeleteModal] = useState("");
 
+  const [docToChange, setDocToChange] = useState("");
+
   const handleOpenDeleteModal = (rowId: string) => {
+    console.log(rowId);
     setOpenDeleteModal(rowId);
   };
 
   const handleDelete = useCallback((row: CartState) => {
     deleteDoc(doc(db, "sales", row.id));
   }, []);
-  const refreshPrice = (sale: CartState) => {
-    let newSales: CartState[] = [];
-    // sales.forEach(async (sale) => {
 
-    let newSale: CartState = {
-      id: "",
-      products: [],
-      total: 0,
-      totalWithDiscount: 0,
-      client: "",
-      date: new Date(0),
-    };
-    let newProducts: Product[] = [];
+  const refreshPrice = (sale: CartState) => {
     sale.products.forEach(async (product) => {
       fetchProductById(product.id, product.cod, product.description)
         .then((product: Product[]) => {
-          console.log(product[0]);
-          console.log(sale);
           updateProduct(sale.id, product[0]);
         })
         .catch((error) => {
@@ -62,14 +51,24 @@ const Counts = () => {
       fetchSales().then((data: CartState[] | null) => {
         console.log(data);
         setsales(data);
-        if (sales && sales.length > 0) {
-          sales?.forEach((s) => refreshPrice(s));
-        }
       });
     }
-  }, [openDeleteModal]);
+    onSnapshot(collection(db, "sales"), (querySnapshot) => {
+      const newSales = FirebaseAdapter.fromDocumentDataArray(
+        querySnapshot.docs
+      );
+      setsales(newSales);
+    });
+  }, []);
   return (
-    <Box mt={2} ml="2rem" mr="2rem" maxWidth={"50"}>
+    <Box
+      mt={2}
+      ml="2rem"
+      mr="2rem"
+      minHeight="100vh"
+      display="flex"
+      flexWrap="wrap"
+    >
       <TableContainer component={Paper}>
         <Table aria-label="collapsible table">
           <TableHead>
@@ -108,6 +107,8 @@ const Counts = () => {
                   handleDeleteDoc={handleDelete}
                   handleOpenDeleteModal={handleOpenDeleteModal}
                   openDeleteModal={openDeleteModal}
+                  setDocToChange={setDocToChange}
+                  refreshPrice={refreshPrice}
                 />
               ))}
           </TableBody>
